06bb647a7147099c47dd93ce77be7fefe32d04bc
FeliOdras <ariane.feldmann@odras.de>
2019-01-14 11:09:20 +0100

    Changed order of functions (best solution first

--- assets/js/happySadLoop.js
+++ assets/js/happySadLoop.js
@@ -10,20 +10,6 @@
     }
 }*/
 
-// Version one with lots of variables 
-let emotionOutput;
-let n = 1;
-let divBy3 = n / 3;
-let divBy5 = n / 5;
-
-for (n = 1; n <= 100; n++) {
-    Number.isInteger(divBy3) && Number.isInteger(divBy5) ? emotionOutput = 'Happy and sad' :
-        Number.isInteger(divBy3) ? emotionOutput = 'Happy' :
-        Number.isInteger(divBy5) ? emotionOutput = 'Sad' :
-        emotionOutput = n;
-    console.log(emotionOutput);
-}
-
 // Version two 
 
 for (n = 1; n <= 100; n++) {
@@ -31,4 +17,16 @@ for (n = 1; n <= 100; n++) {
         Number.isInteger(n / 3) ? console.log(`Happy (${n})`) :
         Number.isInteger(n / 5) ? console.log(`Sad (${n})`) :
         console.log(n);
+}
+
+// Version two with lots of variables 
+let emotionOutput;
+for (n = 1; n <= 100; n++) {
+    let divBy3 = n / 3;
+    let divBy5 = n / 5;
+    Number.isInteger(divBy3) && Number.isInteger(divBy5) ? emotionOutput = 'Happy and sad' :
+        Number.isInteger(divBy3) ? emotionOutput = 'Happy' :
+        Number.isInteger(divBy5) ? emotionOutput = 'Sad' :
+        emotionOutput = n;
+    console.log(emotionOutput);
 }
