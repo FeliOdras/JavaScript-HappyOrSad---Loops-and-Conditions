02d3199b0d33a5b924cc27cc83e211b320b1d1e0
FeliOdras <ariane.feldmann@odras.de>
2019-01-14 11:05:37 +0100

    Created second less DRY solution

--- assets/js/happySadLoop.js
+++ assets/js/happySadLoop.js
@@ -10,6 +10,22 @@
     }
 }*/
 
+// Version one
+let emotionOutput;
+let n = 1;
+let divBy3 = n / 3;
+let divBy5 = n / 5;
+for (n = 1; n <= 100; n++) {
+
+    Number.isInteger(divBy3) && Number.isInteger(divBy5) ? emotionOutput = 'Happy and sad' :
+        Number.isInteger(divBy3) ? emotionOutput = 'Happy' :
+        Number.isInteger(divBy5) ? emotionOutput = 'Sad' :
+        emotionOutput = n;
+    console.log(emotionOutput);
+}
+
+// Version two 
+
 for (n = 1; n <= 100; n++) {
     Number.isInteger(n / 3) && Number.isInteger(n / 5) ? console.log(`Happy and Sad (${n})`) :
         Number.isInteger(n / 3) ? console.log(`Happy (${n})`) :
